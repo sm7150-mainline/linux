7b00a934ace1fd6bd0477cb21736d8eb2f1e489f
Aelin Reidel <aelin@mainlining.org>
2026-03-28 22:58:45 +0100

    WIP WCD stuff

--- drivers/pinctrl/qcom/pinctrl-lpass-lpi.c
+++ drivers/pinctrl/qcom/pinctrl-lpass-lpi.c
@@ -503,6 +503,8 @@ int lpi_pinctrl_probe(struct platform_device *pdev)
 	struct lpi_pinctrl *pctrl;
 	int ret;
 
+	printk(KERN_INFO "lpass-lpi probed successfully\n");
+
 	pctrl = devm_kzalloc(dev, sizeof(*pctrl), GFP_KERNEL);
 	if (!pctrl)
 		return -ENOMEM;
@@ -529,6 +531,8 @@ int lpi_pinctrl_probe(struct platform_device *pdev)
 		if (IS_ERR(pctrl->slew_base))
 			return dev_err_probe(dev, PTR_ERR(pctrl->slew_base),
 					     "Slew resource not provided\n");
+		iowrite32(0x3333, pctrl->slew_base);
+		iowrite32(0xF, pctrl->slew_base + 4);
 	}
 
 	ret = devm_pm_clk_create(dev);
@@ -577,6 +581,8 @@ int lpi_pinctrl_probe(struct platform_device *pdev)
 		dev_err(pctrl->dev, "can't add gpio chip\n");
 		goto err_pinctrl;
 	}
+
+	printk(KERN_INFO "lpass-lpi probed successfully\n");
 
 	return 0;
 
