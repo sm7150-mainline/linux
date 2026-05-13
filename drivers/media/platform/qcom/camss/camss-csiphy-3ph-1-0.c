862bb485c698330cd4a3b9f62b0cec354818fdbb
Aelin Reidel <aelin@mainlining.org>
2026-05-13 02:08:33 +0200

    WIP cam stuff

--- drivers/media/platform/qcom/camss/camss-csiphy-3ph-1-0.c
+++ drivers/media/platform/qcom/camss/camss-csiphy-3ph-1-0.c
@@ -7,6 +7,7 @@
  * Copyright (c) 2011-2015, The Linux Foundation. All rights reserved.
  * Copyright (C) 2016-2018 Linaro Ltd.
  */
+#define DEBUG
 
 #include "camss.h"
 #include "camss-csiphy.h"
@@ -1074,6 +1075,8 @@ static void csiphy_reset(struct csiphy_device *csiphy)
 {
 	struct csiphy_device_regs *regs = csiphy->regs;
 
+	printk(KERN_DEBUG "csiphy_reset id %d with base %p and offset %d\n", csiphy->id, csiphy->base, regs->offset);
+
 	writel_relaxed(0x1, csiphy->base +
 		      CSIPHY_3PH_CMN_CSI_COMMON_CTRLn(regs->offset, 0));
 	usleep_range(5000, 8000);
@@ -1086,6 +1089,8 @@ static irqreturn_t csiphy_isr(int irq, void *dev)
 	struct csiphy_device *csiphy = dev;
 	struct csiphy_device_regs *regs = csiphy->regs;
 	int i;
+
+	printk(KERN_DEBUG "csiphy_isr called\n");
 
 	for (i = 0; i < 11; i++) {
 		int c = i + 22;
@@ -1350,6 +1355,10 @@ static int csiphy_init(struct csiphy_device *csiphy)
 	case CAMSS_6350:
 		regs->lane_regs = &lane_regs_sm6350[0];
 		regs->lane_array_size = ARRAY_SIZE(lane_regs_sm6350);
+		break;
+	case CAMSS_7150:
+		regs->lane_regs = &lane_regs_sm7150[0];
+		regs->lane_array_size = ARRAY_SIZE(lane_regs_sm7150);
 		break;
 	case CAMSS_7280:
 	case CAMSS_8250:
